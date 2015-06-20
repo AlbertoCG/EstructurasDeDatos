f36d5aa821e3f28e7c6a8c5a3dcb046cf7a0d02e
AlbertoCG <alberto.castillo1196@gmail.com>
2015-06-20 16:42:34 -0500

    Añadir y recorrer lista

--- linkedlist/listaenlazada.h
+++ linkedlist/listaenlazada.h
@@ -1,3 +1,9 @@
+#include <iostream>
+#pragma once
+
+template <class T>
+class ListaEnlazada;
+
 template<class T>
 class Nodo {
 private:
@@ -5,8 +11,11 @@ private:
 	T dato;
 
 public:
-	Nodo();
-	friend class ListaEnlazada;
+	Nodo(){	
+		siguiente = NULL;
+	}
+
+	friend class ListaEnlazada<T>;
 };
 
 
@@ -16,5 +25,25 @@ private:
 	Nodo<T>* cabecera;
 
 public:
-	ListaEnlazada();
+	ListaEnlazada(){
+		cabecera = NULL;
+	}
+
+	void anadirInicio(T dato){
+		Nodo<T>* aux;
+		aux = new Nodo<T>();
+		aux->dato = dato;
+		aux->siguiente = this->cabecera;
+		cabecera = aux;
+	}
+
+	void imprimirInicio(){
+		Nodo<T>* aux;
+		aux = cabecera;
+		while(aux != NULL){
+			std::cout << "El dato en: " << aux << " es: " << aux->dato << std::endl;
+			aux = aux->siguiente;
+		};
+	}
 };
+
--- linkedlist/main.cpp
+++ linkedlist/main.cpp
@@ -3,6 +3,11 @@
 
 int main(int argc, char const *argv[])
 {
-	
+	ListaEnlazada<int> lista;
+
+	lista.anadirInicio(5);
+	lista.anadirInicio(4);
+
+	lista.imprimirInicio();
 	return 0;
 }
